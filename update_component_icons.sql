a8c61f54383eb9ff851c8250eaa53283cd3781a8
libra <libra@aiglasses.local>
2025-12-21 22:15:56 +0800

    Update component icons SQL

---
+++ update_component_icons.sql
@@ -0,0 +1,8 @@
+-- 更新组件图标 URL
+UPDATE components 
+SET image_url = 'https://help.hlw.work/images/components/time-display.png'
+WHERE id = '1316892a-da16-4729-a4c0-dd6e10629486';
+
+UPDATE components 
+SET image_url = 'https://help.hlw.work/images/components/realtime-translator.png'
+WHERE id = 'd6a1e91c-9d36-4379-9a27-aa3cd2520d69';
